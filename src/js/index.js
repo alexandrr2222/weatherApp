7c3613dbd029dba624d55a4fdd8839b9ee153ef1
alexandrr2222 <237439209+alexandrr2222@users.noreply.github.com>
2026-05-30 19:24:25 +0200

    Create initial html boilerplate

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,18 @@
 import "../css/reset.css";
 import "../css/global.css";
-console.log("hi")
+console.log("hi");
+
+// commit often
+// branch for major refactor
+// keep dom queries in index
+// nobody imports from each other
+
+// ##########################################
+
+// HEADER: 1. searchbar, 2. settings > f/c, km/miles, 3. current location button, 4. favorite burger menu > all the favorited places
+// MAIN:
+// Right now weather panel:
+// upper part: temp, minmax temp, condition, feels temp
+// lower part details: uv index, sunrise, sunset, chance of rain, humidity, wind
+// Hourly switcher = current hour + 12 with condition icon
+// Day switcher - 5-7 days with average condition icon
